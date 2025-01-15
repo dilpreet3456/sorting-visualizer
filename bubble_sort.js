3187ce49fb2ea951403b8b15eb5363b556c20570
Dilpreet kaur <101334887+dilpreet3456@users.noreply.github.com>
2025-01-15 23:02:04 +0530

    Update bubble_sort.js

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,46 +1,47 @@
 
 
-function Bubble()
-{
-    //Setting Time complexities
-    document.getElementById("Time_Worst").innerText="O(N^2)";
-    document.getElementById("Time_Average").innerText="Θ(N^2)";
-    document.getElementById("Time_Best").innerText="Ω(N)";
+async function Bubble() {
+    // Setting Time Complexities
+    document.getElementById("Time_Worst").innerText = "O(N^2)";
+    document.getElementById("Time_Average").innerText = "Θ(N^2)";
+    document.getElementById("Time_Best").innerText = "Ω(N)";
 
-    //Setting Space complexity
-    document.getElementById("Space_Worst").innerText="O(1)";
+    // Setting Space Complexity
+    document.getElementById("Space_Worst").innerText = "O(1)";
 
-    c_delay=0;
+    c_delay = 0;
 
-    for(var i=0;i<array_size-1;i++)
-    {
-        if (sortingStopped) {
-            sortingStopped = false;
-            generate_array();
-            return;
-          }
-        for(var j=0;j<array_size-i-1;j++)
-        { 
-            //if(sortingStopped)return;//stop the sorting it stop button is clicked
-            div_update(divs[j],div_sizes[j],"yellow");//Color update
-
-            if(div_sizes[j]>div_sizes[j+1])
-            {
-                div_update(divs[j],div_sizes[j], "red");//Color update
-                div_update(divs[j+1],div_sizes[j+1], "red");//Color update
-
-                var temp=div_sizes[j];
-                div_sizes[j]=div_sizes[j+1];
-                div_sizes[j+1]=temp;
-
-                div_update(divs[j],div_sizes[j], "red");//Height update
-                div_update(divs[j+1],div_sizes[j+1], "red");//Height update
+    for (var i = 0; i < array_size - 1; i++) {
+        for (var j = 0; j < array_size - i - 1; j++) {
+            if (sortingStopped) {
+                sortingStopped = false; // Reset the flag
+                enable_buttons(); // Re-enable the buttons
+                return; // Exit the sorting process
             }
-            div_update(divs[j],div_sizes[j], "blue");//Color updat
-        }
-        div_update(divs[j],div_sizes[j], "green");//Color update
-    }
-    div_update(divs[0],div_sizes[0], "green");//Color update
 
-    enable_buttons();
+            div_update(divs[j], div_sizes[j], "yellow"); // Highlight current bar
+
+            if (div_sizes[j] > div_sizes[j + 1]) {
+                div_update(divs[j], div_sizes[j], "red"); // Color update
+                div_update(divs[j + 1], div_sizes[j + 1], "red"); // Color update
+
+                // Swap the elements
+                var temp = div_sizes[j];
+                div_sizes[j] = div_sizes[j + 1];
+                div_sizes[j + 1] = temp;
+
+                // Update heights after swapping
+                div_update(divs[j], div_sizes[j], "red");
+                div_update(divs[j + 1], div_sizes[j + 1], "red");
+            }
+
+            div_update(divs[j], div_sizes[j], "blue"); // Reset color
+            await delay(); // Introduce a delay for visualization
+        }
+
+        div_update(divs[j], div_sizes[j], "green"); // Mark sorted element
+    }
+
+    div_update(divs[0], div_sizes[0], "green"); // Mark the last sorted element
+    enable_buttons(); // Re-enable the buttons after sorting is complete
 }
